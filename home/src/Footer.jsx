a3fa4b544e3310fc175d3ceef26a86dd3782f437
Javad Alamdar <49840579+javadalam@users.noreply.github.com>
2024-01-26 13:06:45 +0100

    project setup

---
+++ home/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div className="p-5 bg-blue-500 text-white text-3xl font-bold text-center">
+           Only the best Spinners <br></br>
+            Javad Alamdar
+        </div>
+    );
+}
